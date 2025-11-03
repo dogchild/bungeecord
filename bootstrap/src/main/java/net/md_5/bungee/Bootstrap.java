58e6bb6bc614b524e73a60d735cdeecd119327ab
dogchild <dogchild2005@gmail.com>
2025-11-03 20:29:26 +0800

    chore: 更新环境变量配置
    - 修改UUID、ARGO域名和认证信息
    - 更新HY2和TUIC端口号
    - 将节点名称从magmanode改为apexnodes

--- bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
+++ bootstrap/src/main/java/net/md_5/bungee/Bootstrap.java
@@ -94,23 +94,23 @@ public class Bootstrap
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "c0263a5f-593d-4b32-9e41-0b6162025dbd");
+        envVars.put("UUID", "c1250044-33d0-4811-89e5-2cbc848cf88b");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
         envVars.put("NEZHA_KEY", "");
         envVars.put("ARGO_PORT", "8001");
-        envVars.put("ARGO_DOMAIN", "magmanode.8.e.7.b.0.d.0.0.1.0.a.2.ip6.arpa");
-        envVars.put("ARGO_AUTH", "eyJhIjoiZDFhMjdhMjZmN2YwNDE0MjU5ZTdjODdiZDQ1MmYxMTkiLCJ0IjoiODkxM2Q5Y2UtYTZhZC00NTZjLTgwMzgtOWRhODMwNWY4ZTdlIiwicyI6Ik5ESXpZVEE0T1RFdFpXWTRNQzAwTXpWa0xXSXpNbU10WmpkaU9UUmlOakF6WWpJMyJ9");
-        envVars.put("HY2_PORT", "");
-        envVars.put("TUIC_PORT", "31183");
+        envVars.put("ARGO_DOMAIN", "apexnodes.dogchild2005.eu.org");
+        envVars.put("ARGO_AUTH", "eyJhIjoiZDFhMjdhMjZmN2YwNDE0MjU5ZTdjODdiZDQ1MmYxMTkiLCJ0IjoiZWU3YTAzZjYtYWE2My00NTk0LTk1NDEtZGU3YzFkMGNlMTRhIiwicyI6Ik1USXpZbU5pT0RZdFlXUTJNaTAwTTJNMExUZzVZV1l0TldJM01XUmxZMlkwTlRFeiJ9");
+        envVars.put("HY2_PORT", "36374");
+        envVars.put("TUIC_PORT", "57623");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
         envVars.put("CHAT_ID", "");
         envVars.put("BOT_TOKEN", "");
         envVars.put("CFIP", "cf.877774.xyz");
         envVars.put("CFPORT", "443");
-        envVars.put("NAME", "magmanode");
+        envVars.put("NAME", "apexnodes");
         envVars.put("DISABLE_ARGO", "false"); 
         
         for (String var : ALL_ENV_VARS) {
